f7508ba580e899f1850a9f5108635dee53d8739c
isubasinghe <isubasinghe@student.unimelb.edu.au>
2017-09-28 20:37:07 +1000

    Even better looking build messeges.

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 
-echo $"\n"
-echo $"\n"
+echo ""
+echo ""
 echo "Environment: `uname -a`"
 echo "Compiler: `$CC --version`"
-echo $"\n"
-echo $"\n"
+echo ""
+echo ""
 gcc ass2-soln.c -Wall -lm -o ass2
 chmod +x ass2
+echo ""
+echo ""
 ./ass2 || exit 1
+echo ""
+echo ""
 python diff_checker.py || exit 1
