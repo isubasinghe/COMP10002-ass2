a2537c5abbb762a37bc1bf59b5104330cedd7cc2
isubasinghe <isubasinghe@student.unimelb.edu.au>
2017-09-28 20:34:23 +1000

    Better looking build messege.

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 
-
+echo $"\n"
+echo $"\n"
 echo "Environment: `uname -a`"
 echo "Compiler: `$CC --version`"
+echo $"\n"
+echo $"\n"
 gcc ass2-soln.c -Wall -lm -o ass2
 chmod +x ass2
 ./ass2 || exit 1
